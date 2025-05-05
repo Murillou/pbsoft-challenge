0e41a94ac56b4d0bd2be6966c87fc92dc60e5fe1
Murillou <murillovinicius13@hotmail.com>
2025-05-05 12:42:48 -0300

    feat(routes): adicionar rotas resource de clients e rota home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ClientController::class, 'index'])->name('home');
+
+Route::resource('clients', ClientController::class);
