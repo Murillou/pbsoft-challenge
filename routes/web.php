86dbf1aee507570faf08ca1c311387f7336af736
Murillou <murillovinicius13@hotmail.com>
2025-05-05 19:19:45 -0300

    refactor(route): renomear nome da rota para dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ClientController::class, 'index'])->name('home');
+Route::get('/', [ClientController::class, 'index'])->name('dashboard');
 
 Route::resource('clients', ClientController::class);
