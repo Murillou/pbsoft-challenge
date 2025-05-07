c8338a8a4c762957ba6a41623e31252ce84d7035
Murillou <murillovinicius13@hotmail.com>
2025-05-06 21:01:11 -0300

    fix(validation): remover biblioteca incompatível com PHP 8.3 e usar regex no lugar

--- app/Http/Requests/ClientValidateRequest.php
+++ app/Http/Requests/ClientValidateRequest.php
@@ -26,7 +26,8 @@ class ClientValidateRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'min:4', 'max:255', 'regex:/^[a-zA-Z\s]+$/'],
             'birthday' => ['required', 'date', 'before:today'],
-            'document' => ['required', 'string', new DocumentValidator, Rule::unique('clients')->ignore($this->client)],
+            'document' => ['required', 'string', 'regex:/^(\d{3}\.?\d{3}\.?\d{3}\-?\d{2}|\d{2}\.?\d{3}\.?\d{3}\/?\d{4}\-?\d{2})$/',
+                Rule::unique('clients')->ignore($this->client)],
             'image_url' => ['nullable', 'image', 'mimes:jpg,jpeg,png', 'max:2048'],
             'social_name' => ['nullable', 'string', 'min:4', 'max:255', 'regex:/^[a-zA-Z\s]+$/']
         ];
